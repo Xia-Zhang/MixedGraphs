4e61b95e8e589d7afc31bd3b51324b83c8c12184
xia zhang <zhangxia9403@gmail.com>
2017-07-04 22:35:24 +0800

    fix some minor error

--- R/BRAIL.R
+++ R/BRAIL.R
@@ -19,9 +19,9 @@ get_c <- function(X_k, beta_k, family) {
     return(max(eigen(t(X_k) %*% W %*% X_k)$values) / max(eigen(t(X_k) %*% X_k)$values))
 }
 
-check_stop_criteria <- function(pre_beta, beta) {
-    result <- mapply(function(a, b) {all(sign(a) == sign(b))}, pre_beta, beta)
-    all(result == TRUE)
+check_stop_criteria <- function(prev_beta, beta) {
+    result <- mapply(function(a, b) {all(sign(a) == sign(b))}, prev_beta, beta)
+    all(result)
 }
 
 #' BRAIL is Block-Randomized Adaptive Iterative Lasso
@@ -52,7 +52,7 @@ BRAIL <- function(X, y, family = "gaussian", tau = 0.8, B = 200, lasso.control =
         family <- "logistic"
 
     while (TRUE) {
-        pre_beta <- beta
+        prev_beta <- beta
         for (k in 1 : K) {
             pk <- ncol(X[[k]])
             beta_norm0 <- sum(abs(sign(beta[[k]])))
@@ -70,16 +70,16 @@ BRAIL <- function(X, y, family = "gaussian", tau = 0.8, B = 200, lasso.control =
                 lasso_argv <- list(X = sample_X[[k]], y = sample_y, o = o, family = family, lambda = w)
                 do.call(glmLasso, c(lasso_argv, lasso.control))$Coef[-1]
             }
-            support_indexes <- which(rowSums(sign(betak_samples))/B >= tau)
+            support_indexes <- which(rowSums(abs(sign(betak_samples)))/B >= tau)
             betak_support <- beta[[k]][support_indexes]
             multi_tmp <-mapply(function(x, y) {x %*% y}, X, beta)
             o <- rowSums(as.matrix(multi_tmp[, -k]))
-            ridge_argv <- list(X = X[[k]][,support_indexes], y = y, o = o, beta.init = pre_beta[[k]][support_indexes], family = family)
+            ridge_argv <- list(X = X[[k]][,support_indexes], y = y, o = o, beta.init = prev_beta[[k]][support_indexes], family = family)
             sub_beta <- do.call(glmRidge, c(ridge_argv, ridge.control))$Coef[-1]
             beta[[k]] <- rep(0, length(beta[[k]]))
             mapply(function(index, value){beta[[k]][index] <<- value}, support_indexes, sub_beta)
         }
-        if (check_stop_criteria(pre_beta, beta)) {
+        if (check_stop_criteria(prev_beta, beta)) {
             break
         }
     }
