076dff325db30f9bdd0c5aee00716989c5f2a635
xia zhang <zhangxia9403@gmail.com>
2017-08-25 05:50:52 +0800

    enrich MixedGraphs-package doc

--- R/MixedGraphs-package.R
+++ R/MixedGraphs-package.R
@@ -1,6 +1,6 @@
-#' MixedGraphs
+#' The package overview
 #' 
-#' A package about mixed graphical models.
+#' @description This package allows for Gaussian, Logistic and Poisson regression with l1 or l2 penalty. And it can also do fitting and visualizing mixed graphical models.
 #'  
 #' @name MixedGraphs-package
 #' @aliases MixedGraphs-package MixedGraphs
@@ -10,13 +10,24 @@
 #' Maintainer: Xia Zhang <zhangxia9403@@gmail.com>
 #' @keywords package
 #' @examples
-#' 
-#' X1 <- matrix(rnorm(12), nrow = 4)
-#' X2 <- matrix(rnorm(12), nrow = 4)
-#' X <- list(X1, X2)
-#' crf_structure <- matrix(c(1, 1, 0, 1), nrow = 2)
+#'
+#' ## regressions
+#' X <- matrix(rnorm(10 * 50), 10, 50)
+#' y <- rnorm(10)
+#' glmLasso(X, y, lambda = 0.5, family = "gaussian", support_stability = 10)
+#' glmRidge(X, y, lambda = 0.5, family = "gaussian", thresh = 0.005)
+#'
+#' ## BRAIL
+#' X <- lapply(1:2, function(x) {matrix(rnorm(5 * 10), 5, 10)})
+#' y <- rnorm(5)
+#' BRAIL(X, y, family = "gaussian", tau = 0.8, B = 20, doPar = TRUE)
+#'
+#' ## MixedGraph fitting and plotting
+#' X <- lapply(1 : 2, function(x){matrix(rnorm(12), nrow = 4)})
+#' crf_structure = matrix(c(1, 0, 1, 1), 2, 2)
 #' brail_control <- list(B = 5, tau = 0.6)
-#' MixedGraph(X, crf_structure, brail_control = brail_control)
+#' G <- MixedGraph(X, crf_structure, brail_control = brail_control)
+#' plot(G, method = "igraph",  weighted = TRUE)
 #' 
 #' @useDynLib MixedGraphs
 #'
